89ca8936ca363969101a7c4b0443e4a68b7af9f7
Julian Ifesiokwu <julianifesiokwu@gmail.com>
2023-02-13 14:47:26 +0100

    Feat: added routing and form submission contexts to project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,56 @@
-import logo from './logo.svg';
+import { useState } from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import SharedLayout from './components/SharedLayout/SharedLayout.component';
+import HomePage from './pages/HomePage/HomePage.Page';
+import AboutPage from './pages/AboutPage/AboutPage.Page';
+import ContactUs from './pages/ContactUsPage/ContactUs.Page';
+import ErrorPage from './pages/ErrorPage/ErrorPage.Page';
+import Gallery from './pages/GalleryPage/Gallery.page';
+import ScrollToTop from './components/ScrollToTop/ScrollToTop.component';
 import './App.css';
+import Volunteer from './pages/VolunteerPage/Volunteer.Page';
+import Donate from './pages/DonatePage/Donate.Page';
+
+import { SubscribeContext } from './context/SubscribeContext';
+import { ContactFormContext } from './context/ContactFormContext';
+import { VolunteerContext } from './context/VolunteerContext';
 
 function App() {
+  const [ data, setData ] = useState({});
+
+  const handleInput = (e) => {
+    let newInput = { [e.target.name]: e.target.value };
+
+    setData({ ...data, ...newInput});
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(data);
+    setData({});
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+    <ScrollToTop />
+    <SubscribeContext.Provider value={{ setData, handleInput, handleSubmit }}>
+      <ContactFormContext.Provider value={{ setData, handleInput, handleSubmit }}>
+        <VolunteerContext.Provider value={{ setData, handleInput, handleSubmit }}>
+            <Routes>
+              <Route path="/" element={<SharedLayout />}>
+                <Route index element={<HomePage />} />
+                <Route path="/about" element={<AboutPage />} />
+                <Route path="/contact-us" element={<ContactUs />} />
+                <Route path="/volunteer" element={<Volunteer />} />
+                <Route path="/donate" element={<Donate />} />
+                <Route path="/gallery" element={<Gallery />} />
+                <Route path="*" element={<ErrorPage />} />
+              </Route>
+            </Routes>
+        </VolunteerContext.Provider>
+      </ContactFormContext.Provider>
+      </SubscribeContext.Provider>
+    </BrowserRouter>
   );
 }
 
