70e03821cc8eb6aea174e01e02af874dad75e500
Julian Ifesiokwu <julianifesiokwu@gmail.com>
2023-02-13 14:50:20 +0100

    created project header and styling

---
+++ src/components/Header/Header.component.jsx
@@ -0,0 +1,39 @@
+import React, { useState } from 'react';
+import { NavLink, Link } from 'react-router-dom'
+import { MdMenu, MdClose } from "react-icons/md";
+import Logo from "../../assets/tabawa_logo.png";
+import './Header.styles.css';
+
+const HeaderAndNavigation = () => {
+  const [menu, showMenu] = useState(false);
+
+  const toggleMenu = () => {
+    showMenu(!menu);
+  };
+
+  return (
+    <header className='header'>
+        <div className="row-two fixed-to-top">
+            <div className="search-section">              
+              <Link to='/'>
+                <div className="logo-container">
+                <img src={Logo} alt="logo" className='logo'/>
+                </div>
+              </Link>
+            <span className='company-name'>Tabawa Foundation</span>
+            {menu ? <MdClose size="35" onClick={toggleMenu} color="#FFF" className='menu-icon'/> : <MdMenu size="35" onClick={toggleMenu} color="#FFF" className='menu-icon'/>}
+            </div>
+            <div className={menu ? "nav-links show" : "nav-links"}>
+                <NavLink to="/" onClick={() => showMenu(!menu)}>Home</NavLink>
+                <NavLink to="/about" onClick={() => showMenu(!menu)}>Who We are?</NavLink>
+                <NavLink to="/contact-us" onClick={() => showMenu(!menu)}>Contact Us</NavLink>
+                <NavLink to="/gallery" onClick={() => showMenu(!menu)}>Gallery</NavLink>
+                <NavLink to="/volunteer" onClick={() => showMenu(!menu)}>Volunteer</NavLink>
+                <NavLink to="/donate" onClick={() => showMenu(!menu)}>Donations</NavLink>
+            </div>
+        </div>
+    </header>
+  )
+};
+
+export default HeaderAndNavigation;
