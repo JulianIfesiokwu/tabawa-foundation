9b982efadf9ef7feeaaceaf750ec5b7f0b93e860
Julian Ifesiokwu <julianifesiokwu@gmail.com>
2023-02-14 16:38:06 +0100

    Feat: changing email input form and size of p elements in footer

--- src/components/Footer/Footer.component.jsx
+++ src/components/Footer/Footer.component.jsx
@@ -12,36 +12,36 @@ const Footer = () => {
   return (
     <div className='footer'>
       <section className="details">
-        <div className="footer-name">
-          <img src={Logo} alt="logo" className='logo' />
-          <p>Tabawa Foundation</p>
+        <div>
+          <div className="footer-name">
+            <img src={Logo} alt="logo" className='logo' />
+          </div>
+          <p className='company-name'>Tabawa Foundation</p>
+        </div>
+        <div className="explore">
+          <h3 className='explore-title'>Navigation</h3>
+          <NavLink to="/">Home</NavLink>
+          <NavLink to="/about">About</NavLink>
+          <NavLink to="/contact-us">Contact</NavLink>
+          <NavLink to="/gallery">Gallery</NavLink>
+          <NavLink to="/donate">Donations</NavLink>
         </div>
         <div className="address">
-          <h3>Contact Information</h3>
+          <h3 className='address-title'>Contact Information</h3>
           <p className='location-info'>109-16 211th St Queens Village Jamaica NY 11429</p>
           <p className='phone-no'>321 Rd F Close Festac Town Lagos Nigeria.</p>
           <p className='phone-no'>+1 3473360734</p>
           <p className='phone-no'>+234 8033209276</p>
           <p className="email">info@tabawafoundation.com</p>
         </div>
-        <div className="explore">
-          <h3 className='explore-title'>Explore</h3>
-          <NavLink to="/">Home</NavLink>
-          <NavLink to="/about">About</NavLink>
-          <NavLink to="/contact-us">Who we are?</NavLink>
-          <NavLink to="/gallery">Gallery</NavLink>
-          <NavLink to="/donate">Donations</NavLink>
-        </div>
         <form onSubmit={(e) => handleSubmit(e)} className="newsletter">
-          <div className='newsletter-title'>
-            <p>Get updates about Tabawa in your inbox</p>
-          </div>
+          <p className='form-update'>Get updates about Tabawa in your inbox</p>
           <input 
             type="email" 
             placeholder='Email' 
             name='subscriber-email' 
             onChange={(e) => handleInput(e)}
-            required/>
+            required />
           <button type="submit" className='newsletter-btn'>Submit</button>
         </form>
     </section>
