57853f14ce703c490ed496d5b227e587a63be037
Julian Ifesiokwu <julianifesiokwu@gmail.com>
2023-02-14 16:40:00 +0100

    Feat: removed cards from homepage, added donation section and more-info section

--- src/pages/HomePage/HomePage.Page.jsx
+++ src/pages/HomePage/HomePage.Page.jsx
@@ -1,11 +1,10 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import carouselImage1 from "../../assets/homepage_landing.jpg";
-import cardImage3 from "../../assets/cardImage (3).jpeg";
-import volunteerImage from "../../assets/volunteer.jpg";
-import donateImage from "../../assets/donate.jpg";
-import trainImage from "../../assets/train.jpg";
-import Card from '../../components/Card/Card.component';
+import cardImage3 from "../../assets/Gallery Images/tabawa (13).jpeg";
+import educationIcon from "../../assets/education.svg";
+import foodIcon from "../../assets/food.svg";
+import medicineIcon from "../../assets/medicine.svg";
 import "./HomePage.Page.styles.css";
 
 const HomePage = () => {
@@ -17,20 +16,18 @@ const HomePage = () => {
             <img src={carouselImage1} alt="" className='landing-image' />
           </div>
           <div className="landing-text">
-            <h1 className='landing-major'>Give a <span className='highlight'>helping hand</span> to those <span className='highlight'>who need it.</span></h1>
-            <Link to="/donate" className='donations-cta'>Donate to a Cause</Link>
+            <h1 className='landing-major'>Give a helping hand to those who need it.</h1>
+            <Link to="/donate" className='donations-cta'>Make a Donation</Link>
           </div>
       </div>
       <section className="welcome">
-        <h2 className="welcome-header">About Us</h2>
         <div className="welcome-content">
           <div className="column-two">
-          <h3 className="welcome-title">Who We Are</h3>
-          <p className="welcome-info">Based in Anambra state, Nigeria, Hungry & Angry is a not for profit Non Government Organisation with the aim to empower, educate and invest in local communities to create and facilitate their development.
+          <h2 className="welcome-title">Who We Are</h2>
+          <p className="welcome-info">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Error, qui! Incidunt dolorum consequuntur, repellendus voluptates accusantium nisi, asperiores, voluptatibus impedit culpa cum tempore inventore soluta atque fuga quibusdam fugiat nihil.
           </p>
           <h3 className="welcome-title">Our Aim</h3>
-          <p className='welcome-info-one'>We are on a mission to help the less priviledged.</p>
-          <p className="welcome-info">We seek to impact local communities with individual grants to persons seeking to develop small businesses, provide training for persons with disabilities, invest in local youths by offering business workshops, training and mentoring.
+          <p className="welcome-info">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Accusamus nisi expedita quod facilis sequi, porro aut voluptas. Sequi deleniti et odit quos dolorem incidunt? Itaque quod commodi illo porro ipsam?
           </p>
           <Link className='about-cta' to='/about'>Learn more...</Link>
         </div>
@@ -40,13 +37,35 @@ const HomePage = () => {
         </div>
       </section>
       <section className="more-info">
-        <h2 className="more-info-header">What We Do</h2>
-        <div className="more-info-content">
-          <Card img={volunteerImage} btnTitle={`Volunteer`} link={`/volunteer`}/>
-          <Card img={trainImage} btnTitle={`Train`} link={`/train`}/>
-          <Card img={donateImage} btnTitle={`Donate`} link={`/donate`}/>
-        </div>
-        </section>
+        <h2 className='more-info-title'>How we help people</h2>
+        <div className='info-details'>
+          <div className='one-item'>
+            <div className='item-img-container'>
+              <img src={educationIcon} alt='education-icon' />
+            </div>
+            <h3 className='item-title'>Education</h3>
+            <p className='item-short'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nostrum pariatur nisi asperiores esse accusamus voluptatibus, praesentium velit iste voluptatum placeat.</p>
+          </div>
+          <div className='one-item'>
+            <div className='item-img-container'>
+              <img src={foodIcon} alt='food-icon' />
+            </div>
+            <h3 className='item-title'>Food</h3>
+            <p className='item-short'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nostrum pariatur nisi asperiores esse accusamus voluptatibus, praesentium velit iste voluptatum placeat.</p>
+          </div>
+          <div className='one-item'>
+            <div className='item-img-container'>
+              <img src={medicineIcon} alt='medicine-icon' />
+            </div>
+            <h3 className='item-title'>Medicine</h3>
+            <p className='item-short'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nostrum pariatur nisi asperiores esse accusamus voluptatibus, praesentium velit iste voluptatum placeat.</p>
+          </div>
+        </div>      
+      </section>
+      <section className='donate-section'>
+        <h2>Help us by donating</h2>
+        <Link to="/donate" className='donations-cta'>Make a Donation</Link> 
+      </section>
     </div>
   )
 };
